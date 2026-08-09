48fed9da003e8315ecedd7bd9cc7139133f0f1fb
Shifat E Rasul <shifaterasulbd@gmail.com>
2026-08-09 17:35:26 +0600

    sd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M9XV5CWZ');
+        </script>
+        <!-- End Google Tag Manager -->
+
         @php
             $metaTitle = trim((string) ($meta['title'] ?? config('app.name', '1971Co')));
             $metaDescription = trim((string) ($meta['description'] ?? 'Discover premium fashion and personalized apparel.'));
@@ -38,6 +48,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body class="bg-[#f4f2ed] text-zinc-950">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9XV5CWZ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div id="app"></div>
     </body>
 </html>
